8d964e6db4e3e5ca5ef888669aed2fffc4df3d07
Hadil24A <hadil.ahmed@hva.nl>
2024-10-18 09:24:43 +0200

    closed #8
    data opgehaald met Fetch( )

---
+++ fabrique-react/src/components/App.jsx
@@ -0,0 +1,11 @@
+import Fetch from './components/Fetch'
+
+const App = ( ) => {
+    return (
+        <div>
+            <Fetch />
+        </div>
+    )
+}
+
+export default App
---
+++ fabrique-react/src/components/Fetch.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+const Fetch = () => {
+    const [artObjects, setArtObjects] = useState([]);
+    useEffect(() => {
+      fetch('https://fdnd-agency.directus.app/items/fabrique_art_objects')
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data);
+          setArtObjects(data.data);
+        });
+    }, []);
+    
+    return (
+        <div>
+          <ul className="masonry">
+            {artObjects.map((art) => (
+              <li className="masonry-item" key={art.id}>
+                <figure>
+                  <img
+                    src={`https://fdnd-agency.directus.app/assets/${art.image}`}
+                    alt={art.title}
+                  />
+                  <figcaption>
+                    <h2>{art.title}</h2>
+                    <a href="#" className="button">
+                      Meer info
+                    </a>
+                  </figcaption>
+                </figure>
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    };
+    
+    export default Fetch;
